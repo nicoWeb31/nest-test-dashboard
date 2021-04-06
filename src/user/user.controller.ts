bec23a9677f389435c23c1258156dd494fb8f7ab
nicoWeb31 <nico.riot@free.fr>
2021-04-06 17:55:51 +0200

    add service and update controller users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,16 @@
 import { Controller, Get } from '@nestjs/common';
+import { User } from './models/user.entity';
+import { UserService } from './user.service';
 
 @Controller('users')
 export class UserController {
 
+    constructor(
+        private userService: UserService,
+    ) {}
+
     @Get()
-    all(){
-        return ['user']
+    async all():Promise<User[]>{
+        return this.userService.all();
     }
 }
