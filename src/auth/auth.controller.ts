65f41b7a5b6294e4ee95481c000235c2e696652f
nicoWeb31 <nico.riot@free.fr>
2021-04-06 18:27:09 +0200

    hash pass

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { User } from 'src/user/models/user.entity';
 import { UserService } from 'src/user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Controller()
 export class AuthController {
@@ -14,7 +15,14 @@ export class AuthController {
 
     @Post('register')
     async register(@Body() body:User): Promise<any> {
-        return this.userService.create(body)
+        const passwordHash = await bcrypt.hash(body.password, 12);
+
+        return this.userService.create({
+            first_name: body.first_name,
+            last_name: body.last_name,
+            mail: body.mail,
+            password: passwordHash,
+        });
         
     }
 }
