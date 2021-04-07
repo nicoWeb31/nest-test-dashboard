d875a39dcdbfd51001e5c5d0697cfa5253bbd9e5
nicoWeb31 <nico.riot@free.fr>
2021-04-07 09:59:38 +0200

    add cookie jwt auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,15 +1,20 @@
-import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { User } from 'src/user/models/user.entity';
 import { UserService } from 'src/user/user.service';
 import * as bcrypt from 'bcryptjs';
 import { RegisterDto } from './models/register.dto';
 import { NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { Request, Response } from 'express';
+import { Req } from '@nestjs/common';
+
 
 @Controller()
 export class AuthController {
 
     constructor(
         private userService: UserService,
+        private jwtService: JwtService,
     ) {
 
     }
@@ -37,6 +42,7 @@ export class AuthController {
     async login(
         @Body('mail') mail: string,
         @Body('password') password: string,
+        @Res({passthrough: true}) response : Response
     ) {
 
         //searhc user
@@ -52,7 +58,19 @@ export class AuthController {
             throw new BadRequestException('Invalid credentials');
         }
 
-        return user
+        const jwt = await this.jwtService.signAsync({id: user.id})
+
+        response.cookie('jwt', jwt, {httpOnly: true});
+    }
+
+    //get User by jwt
+    @Get('user')
+    async user(@Req() request: Request){
+        const cookie = request.cookies['jwt'];
+        const data = await this.jwtService.verifyAsync(cookie);
+        return data;
+
+
     }
 
 }
